87dd7c1ce47a7211ba903390ad6d5fa63e3060ba
treewx <your-email@example.com>
2025-12-23 10:53:44 +1300

    Set up GitHub Actions for PWA deployment

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -2,7 +2,7 @@ javascript: (function () {
     var text = document.body.innerText;
     var url = window.location.href;
     var title = document.title;
-    var baseUrl = "https://nasty-apples-double.loca.lt/";
+    var baseUrl = "https://treewx.github.io/floatywords/";
     var finalUrl = baseUrl + "?title=" + encodeURIComponent(title) + "&text=" + encodeURIComponent(text.substring(0, 2000)) + "&url=" + encodeURIComponent(url);
     window.open(finalUrl, '_blank');
 })();
